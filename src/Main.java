6a2c1a45983c535c9f39bd93dea100de369cc09f
CarlosAUD <150806786+CarlosAUD@users.noreply.github.com>
2024-02-02 12:13:15 -0500

    Aqui añadimos la parte del banco

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("EL BUEN BUHO");
+        frame.setContentPane(new Transaccion().transaccion);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
--- src/Saldo.java
+++ src/Saldo.java
@@ -1,2 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Saldo {
+    public JPanel saldo;
+    private JButton MENUButton;
+    public Transaccion transaccion;
+
+    public Saldo() {
+        MENUButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(MENUButton);
+                frame.dispose();
+                JFrame frames = new JFrame("QUE TRANSACCION VA A REALIZAR");
+                frames.setContentPane(new Transaccion().transaccion);
+                frames.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frames.setSize(300, 300);
+                frames.pack();
+                frame.setLocationRelativeTo(null);
+                frames.setVisible(true);
+            }
+        });
+    }
 }
